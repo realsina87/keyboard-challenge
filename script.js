9441ff1f2841e5e8b2003c624805842b8d038fe8
Sina Rokni <realsina87@gmail.com>
2022-08-03 22:08:11 +0430

    a bug fixed

--- script.js
+++ script.js
@@ -1,209 +1,33 @@
 const
-q = document.querySelector(".q") ,
-w = document.querySelector(".w") ,
-e = document.querySelector(".e") ,
-r = document.querySelector(".r") ,
-t = document.querySelector(".t") , 
-y = document.querySelector(".y") , 
-u = document.querySelector(".u") , 
-i = document.querySelector(".i") ,
-o = document.querySelector(".o") ,
-p = document.querySelector(".p") ,
+div = document.querySelectorAll("div") ;
 
+let arr = Array.from(div);
+arr.shift();
 
-a = document.querySelector(".a") , 
-s = document.querySelector(".s") , 
-d = document.querySelector(".d") ,
-f = document.querySelector(".f") ,
-g = document.querySelector(".g") ,
-h = document.querySelector(".h") ,
-j = document.querySelector(".j") , 
-k = document.querySelector(".k") ,
-l = document.querySelector(".l") ,
+let lowerCase = [];
 
-z = document.querySelector(".z") ,
-x = document.querySelector(".x") ,
-c = document.querySelector(".c") ,
-v = document.querySelector(".v") ,
-b = document.querySelector(".b") ,
-n = document.querySelector(".n") ,
-m = document.querySelector(".m") ;
+for(var i = 0 ; i < arr.length ; i++){
+    var low = arr[i].innerHTML.toLowerCase();
+    lowerCase.push(low);
+}
 
-
+console.log(lowerCase[1]);
+console.log(arr);
 
 
 document.addEventListener("keydown" , (event) => {
-    if(event.key == 'q' || event.key == 'Q'){
-        q.classList.toggle("active");
-    }
-    else if(event.key == 'w' || event.key == 'W'){
-        w.classList.toggle("active");
-    }
-    else if(event.key == 'e' || event.key == 'e'){
-        e.classList.toggle("active");
-    }
-    else if(event.key == 'r' || event.key == 'r'){
-        r.classList.toggle("active");
-    }
-    else if(event.key == 't' || event.key == 't'){
-        t.classList.toggle("active");
-    }
-    else if(event.key == 'y' || event.key == 'y'){
-        y.classList.toggle("active");
-    }
-    else if(event.key == 'u' || event.key == 'u'){
-        u.classList.toggle("active");
-    }
-    else if(event.key == 'i' || event.key == 'i'){
-        i.classList.toggle("active");
-    }
-    else if(event.key == 'o' || event.key == 'o'){
-        o.classList.toggle("active");
-    }
-    else if(event.key == 'p' || event.key == 'p'){
-        p.classList.toggle("active");
-    }
+    for(var j = 0 ; j <= 25 ; j++){
 
-
-    else if(event.key == 'a' || event.key == 'a'){
-        a.classList.toggle("active");
+        if(event.key == arr[j].innerHTML || event.key == lowerCase[j]){
+            arr[j].classList.toggle("active")
+        } 
     }
-    else if(event.key == 's' || event.key == 's'){
-        s.classList.toggle("active");
-    }
-    else if(event.key == 'd' || event.key == 'd'){
-        d.classList.toggle("active");
-    }
-    else if(event.key == 'f' || event.key == 'f'){
-        f.classList.toggle("active");
-    }
-    else if(event.key == 'g' || event.key == 'g'){
-        g.classList.toggle("active");
-    }
-    else if(event.key == 'h' || event.key == 'h'){
-        h.classList.toggle("active");
-    }
-    else if(event.key == 'j' || event.key == 'j'){
-        j.classList.toggle("active");
-    }
-    else if(event.key == 'k' || event.key == 'k'){
-        k.classList.toggle("active");
-    }
-    else if(event.key == 'l' || event.key == 'l'){
-        l.classList.toggle("active");
-    }
-
-
-    else if(event.key == 'z' || event.key == 'z'){
-        z.classList.toggle("active");
-    }
-    else if(event.key == 'x' || event.key == 'x'){
-        x.classList.toggle("active");
-    }
-    else if(event.key == 'c' || event.key == 'c'){
-        c.classList.toggle("active");
-    }
-    else if(event.key == 'v' || event.key == 'v'){
-        v.classList.toggle("active");
-    }
-    else if(event.key == 'v' || event.key == 'v'){
-        v.classList.toggle("active");
-    }
-    else if(event.key == 'b' || event.key == 'b'){
-        b.classList.toggle("active");
-    }
-    else if(event.key == 'n' || event.key == 'n'){
-        n.classList.toggle("active");
-    }
-    else if(event.key == 'm' || event.key == 'm'){
-        m.classList.toggle("active");
-    }
-})
+});
 document.addEventListener("keyup" , (event) => {
-    if(event.key == 'q' || event.key == 'Q'){
-        q.classList.toggle("active");
-    }
-    else if(event.key == 'w' || event.key == 'W'){
-        w.classList.toggle("active");
-    }
-    else if(event.key == 'e' || event.key == 'e'){
-        e.classList.toggle("active");
-    }
-    else if(event.key == 'r' || event.key == 'r'){
-        r.classList.toggle("active");
-    }
-    else if(event.key == 't' || event.key == 't'){
-        t.classList.toggle("active");
-    }
-    else if(event.key == 'y' || event.key == 'y'){
-        y.classList.toggle("active");
-    }
-    else if(event.key == 'u' || event.key == 'u'){
-        u.classList.toggle("active");
-    }
-    else if(event.key == 'i' || event.key == 'i'){
-        i.classList.toggle("active");
-    }
-    else if(event.key == 'o' || event.key == 'o'){
-        o.classList.toggle("active");
-    }
-    else if(event.key == 'p' || event.key == 'p'){
-        p.classList.toggle("active");
-    }
+    for(var j = 0 ; j <= 25 ; j++){
 
-
-    else if(event.key == 'a' || event.key == 'a'){
-        a.classList.toggle("active");
+        if(event.key == arr[j].innerHTML || event.key == lowerCase[j]){
+            arr[j].classList.toggle("active")
+        } 
     }
-    else if(event.key == 's' || event.key == 's'){
-        s.classList.toggle("active");
-    }
-    else if(event.key == 'd' || event.key == 'd'){
-        d.classList.toggle("active");
-    }
-    else if(event.key == 'f' || event.key == 'f'){
-        f.classList.toggle("active");
-    }
-    else if(event.key == 'g' || event.key == 'g'){
-        g.classList.toggle("active");
-    }
-    else if(event.key == 'h' || event.key == 'h'){
-        h.classList.toggle("active");
-    }
-    else if(event.key == 'j' || event.key == 'j'){
-        j.classList.toggle("active");
-    }
-    else if(event.key == 'k' || event.key == 'k'){
-        k.classList.toggle("active");
-    }
-    else if(event.key == 'l' || event.key == 'l'){
-        l.classList.toggle("active");
-    }
-
-
-    else if(event.key == 'z' || event.key == 'z'){
-        z.classList.toggle("active");
-    }
-    else if(event.key == 'x' || event.key == 'x'){
-        x.classList.toggle("active");
-    }
-    else if(event.key == 'c' || event.key == 'c'){
-        c.classList.toggle("active");
-    }
-    else if(event.key == 'v' || event.key == 'v'){
-        v.classList.toggle("active");
-    }
-    else if(event.key == 'v' || event.key == 'v'){
-        v.classList.toggle("active");
-    }
-    else if(event.key == 'b' || event.key == 'b'){
-        b.classList.toggle("active");
-    }
-    else if(event.key == 'n' || event.key == 'n'){
-        n.classList.toggle("active");
-    }
-    else if(event.key == 'm' || event.key == 'm'){
-        m.classList.toggle("active");
-    }
-    }
-);
+});
